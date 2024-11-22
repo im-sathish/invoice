a10f21a9035a2dffca4dcc4f5bd9d448c109a0c1
Sathish kumar S <117337698+im-sathish@users.noreply.github.com>
2024-11-22 15:51:27 +0530

    Update server.js

--- server.js
+++ server.js
@@ -112,7 +112,7 @@ app.get('*', (req, res) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
